277ba12fb74fd0646ada62e5ece45edf0abbae6a
Sandra Szenti <alexandraszenti@gmail.com>
2015-05-03 07:27:15 +0100

    perf optimisation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
 	document.querySelector( "#nav-toggle" )
-		.addEventListener( "click", function() {
-		this.parentElement.classList.toggle( "active" );
-	});
+		.addEventListener( "click", function(e) {
+			e.preventDefault();
+			this.parentElement.classList.toggle( "active" );
+		});
 	var i,
 		otherLinks = document.querySelectorAll( "nav a:not(#nav-toggle)" ),
 		len = otherLinks.length;
 
 		for (i = 0; i < len; i++ ) {
 			otherLinks[i].addEventListener( "click", function() {
-				console.log('test');
 				if(this.parentElement.className.length > 0) {
 					this.parentElement.className = ""
 				}
